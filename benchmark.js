f0f9249757678e9fb31271479d6b8627260b6623
Soho Tetsuobe <tetsuobe@gmail.com>
2020-01-30 12:23:51 +0100

    sample and bench

---
+++ benchmark.js
@@ -0,0 +1,55 @@
+const Benchmark = require('benchmark')
+const run = require('./samples')
+
+const suite = new Benchmark.Suite;
+ 
+// add tests
+suite
+.add('[crypto] hash (sha1)', function() {
+    run.cryptoHash('sha1')
+})
+.add('[crypto] hash (sha512)', function() {
+    run.cryptoHash('sha512')
+})
+.add('[crypro] Hmac (sha1 with global salt)', function() {
+    run.cryptoHmac('sha1')
+})
+.add('[crypro] Hmac (sha512 with global salt)', function() {
+    run.cryptoHmac('sha512')
+})
+.add('[crypro] Hmac (sha1 with hashed salt)', function() {
+    run.cryptoHmacHashSalt('sha1')
+})
+.add('[crypro] Hmac (sha512 with hashed salt)', function() {
+    run.cryptoHmacHashSalt('sha512')
+})
+.add('[crypto] pbkdf2 with global salt', function() {
+    run.cryptoPbkdf2('sha512')
+})
+.add('[crypto] pbkdf2 with hashed salt', function() {
+    run.cryptoPbkdf2HashSalt('sha512')
+})
+.add('[bcrypt] hash with global salt', function() {
+    run.bcryptHash()
+})
+// add listeners
+.on('start', function() { 
+    console.log('Benchmark started');
+})
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('abort', function() {
+  console.log('Benchmark aborted');
+})
+.on('error', function(error) {
+  console.log(error)
+})
+.on('reset', function() {
+  console.log('reset')
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+// run async
+.run({ 'async': true });
---
+++ samples.js
@@ -0,0 +1,63 @@
+const crypto = require('crypto')
+const bcrypt = require('bcrypt')
+
+const token = '123456789012'
+const salt = '$2b$10$BR8RufwrXQWEe6uOT0EB8u'
+
+function hashSalt() {
+    return crypto.createHash('sha1').update(salt + token).digest('hex')
+}
+
+function cryptoHash(algorithm) {
+    return crypto.createHash(algorithm).update(token).digest('hex');
+}
+function cryptoHmac(algorithm) {
+    return crypto.createHmac(algorithm, salt).update(token).digest('hex');   
+}
+function cryptoHmacHashSalt(algorithm) {
+    return crypto.createHmac(algorithm, hashSalt()).update(token).digest('hex');   
+}
+function bcryptHash() {
+    return bcrypt.hashSync(token, salt)
+}
+function cryptoPbkdf2(algorithm) {
+    return crypto.pbkdf2Sync(token, salt, 1000, 64, algorithm).toString('hex')
+}
+function cryptoPbkdf2HashSalt(algorithm) {
+    return crypto.pbkdf2Sync(token, hashSalt(), 1000, 64, algorithm).toString('hex')
+}
+
+module.exports = {
+    cryptoHash,
+    cryptoHmac,
+    cryptoHmacHashSalt,
+    bcryptHash,
+    cryptoPbkdf2,
+    cryptoPbkdf2HashSalt
+}
+
+console.log(
+    "Examples of hashing: \n" +
+    "Token: '" + token + "' \n" +
+    "Salt '" + salt + "' \n" +
+    "Hashed salt '" + hashSalt() + "' \n" +
+    "=============================\n" +
+    "[crypto] hash (sha1) {length: " + cryptoHash('sha1').length + " chars}: \n" + cryptoHash('sha1') + "\n" +
+    "-----------------------------\n" +
+    "[crypto] hash (sha512) {length: " + cryptoHash('sha512').length + " chars}: \n" + cryptoHash('sha512') + "\n" +
+    "-----------------------------\n" +
+    "[crypto] hmac (sha1 with global salt) {length: " + cryptoHmac('sha1').length + " chars}: \n" + cryptoHmac('sha1') + "\n" +
+    "-----------------------------\n" +
+    "[crypto] hmac (sha512 with global salt) {length: " + cryptoHmac('sha512').length + " chars}: \n" + cryptoHmac('sha512') + "\n" +
+    "-----------------------------\n" +
+    "[crypto] hmac (sha1 with hashed salt) {length: " + cryptoHmacHashSalt('sha1').length + " chars}: \n" + cryptoHmacHashSalt('sha1') + "\n" +
+    "-----------------------------\n" +
+    "[crypto] hmac (sha512 with hashed salt) {length: " + cryptoHmacHashSalt('sha512').length + " chars}: \n" + cryptoHmacHashSalt('sha512') + "\n" + 
+    "-----------------------------\n" +
+    "[crypto] pbkdf2 (sha512 with global salt) {length: " + cryptoPbkdf2('sha512').length + " chars}: \n" + cryptoPbkdf2('sha512') + " \n" +
+    "-----------------------------\n" +
+    "[crypto] pbkdf2 (sha512 with hashed salt) {length: " + cryptoPbkdf2HashSalt('sha512').length + " chars}: \n" + cryptoPbkdf2HashSalt('sha512') + " \n" +
+    "-----------------------------\n" +
+    "[bcrypt] hash {length: " + bcryptHash().length + " chars}: \n" + bcryptHash() + "\n" + 
+    "=============================\n\n"
+)
